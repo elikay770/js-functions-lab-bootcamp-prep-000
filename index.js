c40aac8b3e696ecaa6581e8d1b08976287c95245
Elliot Kahan <ejkahan@gmail.com>
2017-11-04 17:43:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
     return `Happy holidays, you!`
   }
-function happyHolidayTO(holiday, name) {
+function happyHolidayTo(holiday, name) {
 return `Happy $(holiday) , $(name)!`
 }
